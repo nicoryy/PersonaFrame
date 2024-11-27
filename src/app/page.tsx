8b85abe01709a94c825b88a8c42ab6f824dd3a39
nicoryy <pedronicory@gmail.com>
2024-11-27 11:35:56 -0300

    initiating route pages design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,19 @@
-import { WindSong, Raleway } from "next/font/google";
-import { FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
-
-const windSong = WindSong({ subsets: ["latin"], weight: ["400", "500"] });
-const raleway = Raleway({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
 export default function Home() {
   return (
-    <section>
-      <div className="flex justify-between py-5 px-10 mb-20">
-        <p className={`text-3xl font-medium ${windSong.className}`}>
-          Pedro <span className="gradient">Nicory</span>
-        </p>
-
-        <button className="btn btn-sm">Moon</button>
+    <section className="mx-10 text-left">
+      <div className="flex gap-5 w-full items-center justify-start">
+        <p className="uppercase text-3xl">about me</p>
+        <span className="divider w-96" />
       </div>
 
-      <section className="mx-20 flex gap-5">
-        {/* conteiner 1 */}
-        <div className="bg-[#ffffff] min-w-96 text-center p-2 rounded-lg grid grid-cols-1 gap-2">
-          {/* IMAGEM */}
-          <span className="bg-primary w-40 h-40 block mx-auto rounded-lg -mt-20 mb-10"></span>
+      <p className="text-sm">
+        Hello there! I'm thrilled to welcome you to my portfolio. I am a passionate and versatile
+        full-stack developer with a keen interest in exploring the latest cutting-edge technologies.
+        My journey in the world of web development has been nothing short of exhilarating, and I
+        constantly strive to enhance my skills and embrace emerging trends in the industry.
+      </p>
 
-          <h1 className={`font-semibold text-2xl ${raleway.className}`}>
-            Pedro Nicory
-          </h1>
-          <h2 className="text-sm text-gray-400 ">FullStack Developer</h2>
-
-          <section className="flex gap-2 justify-center">
-            <a href='/' className="btn btn-md"><FaLinkedin size={20}/></a>
-            <a href='/' className="btn btn-md"><FaGithub size={20}/></a>
-            <a href='/' className="btn btn-md"><FaInstagram size={20}/></a>
-            <a href='/' className="btn btn-md"><FaXTwitter size={20}/></a>
-          </section>
-
-          <section className="">
-
-          </section>
-
-        </div>
-
-        {/* conteiner 2 */}
-        <div className="bg-[#ffffff] w-full text-center p-2 rounded-lg">
-          <p>About me</p>
-        </div>
-      </section>
+      <p className="text-2xl">What i do!</p>
     </section>
   );
 }
