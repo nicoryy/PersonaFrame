72e61626de48cd57e9076481c13b77fff69d0a91
pedronicory <pedronicory@gmail.com>
2024-11-29 14:51:00 +0000

    feat:loading page animation

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+async function loading() {
+  return (
+    <section className="flex justify-center items-center p-20">
+      <span className="loading loading-dots loading-lg"></span>
+    </section>
+  );
+}
+
+export default loading;
