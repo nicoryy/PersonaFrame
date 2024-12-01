faceb4f5701829950d3b64bb3429967d6fd6ff33
pedronicory <pedronicory@gmail.com>
2024-12-01 15:10:36 +0000

    feat: finished reusme page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -14,7 +14,7 @@ function ResumePage() {
         {/* education */}
         <section className="grid gap-5">
           <IconTitle icon={<BsMortarboard size={20} />} title="Education" />
-          <div className="p-3 bg-primary flex flex-col justify-between rounded-lg">
+          <div className="p-3 bg-primary text-white dark:text-neutral-content flex flex-col justify-between rounded-lg">
             <p className="text-sm">2021-2022</p>
             <p className="font-semibold my-2">Bachelor in Computer Science</p>
             <p className="text-sm">Harvard</p>
@@ -36,7 +36,7 @@ function ResumePage() {
             <p className="text-sm">Brazil Instituition</p>
           </div>
 
-          <div className="p-3 bg-primary flex flex-col justify-between rounded-lg">
+          <div className="p-3 bg-primary text-white dark:text-neutral-content flex flex-col justify-between rounded-lg">
             <p className="text-sm">2021-2022</p>
             <p className="font-semibold my-2">Bachelor in Computer Science</p>
             <p className="text-sm">Harvard</p>
@@ -46,8 +46,54 @@ function ResumePage() {
 
       {/* work skill | soft skill */}
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-        <section>
-          <IconTitle icon={<FaBriefcase size={20} />} title="Work Skill" />
+        <section className="grid gap-5">
+          <IconTitle title="Work Skill" />
+
+          <div className="flex flex-wrap gap-5">
+            {[
+              "NEXT.js",
+              "React.js",
+              "HTML 5",
+              "CSS 3",
+              "Tailwind CSS",
+              "Figma",
+              "JavaScript",
+              "Mongo DB",
+              "SQL",
+              "Angular",
+              "Android",
+              "Git",
+            ].map((skill) => (
+              <p
+                key={skill}
+                className="text-sm py-1 px-3 bg-neutral rounded-full badge-neutral text-black dark:text-neutral-content whitespace-nowrap"
+              >
+                {skill}
+              </p>
+            ))}
+          </div>
+        </section>
+
+        <section className="grid gap-5">
+          <IconTitle title="Soft Skills" />
+
+          <div className="flex flex-wrap gap-5 place-items-center">
+            {[
+              "Time Management",
+              "Mentorship",
+              "Impeccable Communication",
+              "Flexibility",
+              "Research",
+              "Writing",
+            ].map((skill) => (
+              <p
+                key={skill}
+                className="text-sm py-1 px-3 bg-neutral rounded-full badge-neutral text-black dark:text-neutral-content whitespace-nowrap"
+              >
+                {skill}
+              </p>
+            ))}
+          </div>
         </section>
       </section>
     </Conteiner>
