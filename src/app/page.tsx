760cae0edbcb084705fa092d97a71f3f82f06e9c
Nicory <pedronicory@gmail.com>
2024-12-30 10:53:29 -0300

    "feat: Update Home page with atom-based content"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,66 @@
+"use client";
+
+// Components
 import Conteiner from "@/components/layout/Conteiner";
 import IconTitle from "@/components/layout/IconTitle";
 import Title from "@/components/layout/Title";
+
+// Atoms
+import { homeAtom } from "@/components/constans";
+import { useAtom } from "jotai";
+
+// Icons
 import { FaUserFriends } from "react-icons/fa";
 import { FaLaptopCode, FaMobile, FaPaintbrush } from "react-icons/fa6";
 
 export default function Home() {
+  const [homePage] = useAtom(homeAtom);
+
   return (
     <Conteiner>
       <Title title="About me" />
-      <p className="text-sm">
-        Hello there! I&lsquo;m thrilled to welcome you to my portfolio. I am a
-        passionate and versatile full-stack developer with a keen interest in
-        exploring the latest cutting-edge technologies. My journey in the world
-        of web development has been nothing short of exhilarating, and I
-        constantly strive to enhance my skills and embrace emerging trends in
-        the industry.
-      </p>
+      <p className="text-sm">{homePage.aboutme}</p>
 
       <p className="text-2xl">What i do!</p>
 
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         <div className="bg-primary text-white dark:text-neutral-content rounded-lg p-5">
-          <IconTitle title="Development" icon={<FaLaptopCode size={20} />} />
+          <IconTitle
+            title={homePage.whatido.section1.title}
+            icon={<FaLaptopCode size={20} />}
+          />
           <p className="text-sm text-justify">
-            As a developer, I find myself most captivated by the power and
-            flexibility of NEXT.js. I&lsquo;m always eager to dive into new projects
-            that leverage NEXT.js and discover innovative ways to create fast,
-            scalable, and user-friendly applications.
+            {homePage.whatido.section1.description}
           </p>
         </div>
 
         <div className="bg-base-200 rounded-lg p-5">
-          <IconTitle title="Mobile" icon={<FaMobile size={20} />} />
+          <IconTitle
+            title={homePage.whatido.section2.title}
+            icon={<FaMobile size={20} />}
+          />
           <p className="text-sm text-justify">
-            With a focus on user-centric design and cutting-edge technologies, I
-            thrive on building intuitive and efficient apps that make a positive
-            impact on people&lsquo;s lives. Let&lsquo;s turn ideas into reality and shape
-            the future together.
+            {homePage.whatido.section2.description}
           </p>
         </div>
 
         <div className="bg-base-200 rounded-lg p-5">
-          <IconTitle title="Design" icon={<FaPaintbrush size={20} />} />
+          <IconTitle
+            title={homePage.whatido.section3.title}
+            icon={<FaPaintbrush size={20} />}
+          />
           <p className="text-sm text-justify">
-            Crafting visually appealing and intuitive user interfaces that offer
-            a delightful user experience is something I&lsquo;m truly fanatic about.
+            {homePage.whatido.section3.description}
           </p>
         </div>
 
         <div className="bg-primary text-white dark:text-neutral-content rounded-lg p-5">
-          <IconTitle title="Mentorship" icon={<FaUserFriends size={20} />} />
+          <IconTitle
+            title={homePage.whatido.section4.title}
+            icon={<FaUserFriends size={20} />}
+          />
           <p className="text-sm text-justify">
-            I have also found great joy in sharing my knowledge with others.
-            Being a technical mentor allows me to give back to the community
-            that has supported me throughout my career.
+            {homePage.whatido.section4.description}
           </p>
         </div>
       </section>
