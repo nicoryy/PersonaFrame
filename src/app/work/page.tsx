f11574e69555fa9c79d5bdebddab86367a869d68
Nicory <pedronicory@gmail.com>
2024-12-30 21:46:47 -0300

    "feat: Add github link to work projects"

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -21,14 +21,23 @@ const WorkPage = () => {
       </ul>
 
       <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-        {work.projects.map(({ image, title, type }, index) => (
+        {work.projects.map(({ image, title, type, github }, index) => (
           <div className="bg-base-200 p-5 grid gap-2 rounded-lg" key={index}>
             {/* Change this with your image */}
             <div className="w-full h-40 bg-base-300 rounded-lg    grid place-items-center  ">
               {image}
             </div>
             <p className="text-sm font-mono">{type}</p>
-            <h3 className="font-semibold text-lg">{title}</h3>
+            <div className="flex justify-between">
+              <h3 className="font-semibold text-lg">{title}</h3>
+              <a
+                href={github}
+                target="_blank"
+                className="btn btn-sm btn-primary"
+              >
+                Details
+              </a>
+            </div>
           </div>
         ))}
       </section>
