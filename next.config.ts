3826a6d1007698b0bcd337b46c5d0341ca128ef6
Nicory <pedronicory@gmail.com>
2025-01-09 08:51:22 -0300

    Fix: Update Next.js to latest version (15.1.4) and change image domains configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**", // Permite todos os caminhos no domínio especificado
+      },
+    ],
   },
 };
 
