bff65e7a539822b18020da26d874f451e5a4e958
Nicory <pedronicory@gmail.com>
2025-01-10 00:14:52 -0300

    Refactor Title component - Optimize gradient width"

--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -4,7 +4,7 @@ function Title({title}: {title: string}) {
   return (
     <div className="flex gap-5 w-full items-center justify-start">
       <p className="uppercase text-nowrap text-2xl font-semibold">{title}</p>
-      <span className="bg-gradient-to-r from-primary to-secondary w-full lg:w-96 block h-0.5" />
+      <span className="bg-gradient-to-r from-primary to-secondary w-full block h-0.5" />
     </div>
   );
 }
